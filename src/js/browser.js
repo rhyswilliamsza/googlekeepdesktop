606d7188bd6deef1e44ab149d9fa2db2c243020b
Rhys Williams <me@rhyswilliams.co.za>
2017-06-23 22:11:42 +0200

    Version 1.0.0

--- src/js/browser.js
+++ src/js/browser.js
@@ -6,17 +6,16 @@
 /**
  * Do actions on window load
  */
+
 window.onload = function () {
     const webview = document.querySelector('webview');
     const loading = document.querySelector('#loading');
     const lightBulb = document.querySelector("#lightBulb");
 
     const loadstop = () => {
-
         //Shift Down Top Bar
         webview.insertCSS("#gb > div:nth-child(2) { padding-top: 20px })");
-
-        webview.executeJavaScript('//Add Version\nvar original = document.getElementsByClassName("hSRGPd-haAclf")[0].innerHTML;\nif (!original.includes("Google Keep Desktop")) {\n    document.getElementsByClassName("hSRGPd-haAclf")[0].innerHTML = "<p style=\\"color: #999; font-size: 12px; padding: 0px 5px; text-decoration: none; white-space: nowrap;\\">Google Keep Desktop V0.0.1</p>" + original;\n}\n\n//Remove Apps Drawer\ndocument.querySelector("[aria-label=\'Google apps\']").style.display = "none";');
+        webview.executeJavaScript('//Add Version\nvar original = document.getElementsByClassName("hSRGPd-haAclf")[0].innerHTML;\nif (!original.includes("Google Keep Desktop")) {\n    document.getElementsByClassName("hSRGPd-haAclf")[0].innerHTML = "<p style=\\"color: #999; font-size: 12px; padding: 0px 5px; text-decoration: none; white-space: nowrap;\\">Google Keep Desktop V1.0.0</p>" + original;\n}\n\n//Remove Apps Drawer\ndocument.querySelector("[aria-label=\'Google apps\']").style.display = "none";');
 
         //Loadout
         loading.style.animation = "loadFadeOut 0.7s forwards";
