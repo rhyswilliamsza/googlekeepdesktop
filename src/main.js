7c4547b93b500d0990df6382d9338b51228f5a5f
Rhys Williams <me@rhyswilliams.co.za>
2017-06-23 21:28:05 +0200

    Fixed Favicon

--- src/main.js
+++ src/main.js
@@ -6,6 +6,9 @@ const url = require('url');
 
 var mainWindow;
 
+/**
+ * Create Window
+ */
 function createWindow() {
     // Create the browser window.
     mainWindow = new BrowserWindow(
@@ -34,27 +37,22 @@ function createWindow() {
     })
 }
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
+/**
+ * Do actions on ready
+ */
 app.on('ready', createWindow);
 
 app.title = "Google Keep";
 
 app.commandLine.appendSwitch('ignore-gpu-blacklist');
 
-// Quit when all windows are closed.
 app.on('window-all-closed', function () {
-    // On OS X it is common for applications and their menu bar
-    // to stay active until the user quits explicitly with Cmd + Q
     if (process.platform !== 'darwin') {
         app.quit()
     }
 });
 
 app.on('activate', function () {
-    // On OS X it's common to re-create a window in the app when the
-    // dock icon is clicked and there are no other windows open.
     if (mainWindow === null) {
         createWindow()
     } else {
